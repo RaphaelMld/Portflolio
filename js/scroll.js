be51418eb778c33fa2d00e6c9ba770ae19ae9326
RaphaelM <malidin.raphael@gmail.com>
2024-05-04 17:36:46 +0200

    modif

--- js/scroll.js
+++ js/scroll.js
@@ -21,74 +21,130 @@ btn1.addEventListener('click', () => {
 
 const btn2 = document.querySelector('.btnEtudes');
 btn2.addEventListener('click', () => {
-    if (window.innerWidth > 830) {
+    if (window.innerWidth > 1150) {
         window.scrollTo({
-            top: 1450,
+            top: 1400,
             left: 0,
             behavior: "smooth"
-        
-        })
+        });
     } else {
-        if (window.innerWidth > 400) {
+        if (window.innerWidth > 960) {
             window.scrollTo({
-                top: 1900,
+                top: 1500,
                 left: 0,
                 behavior: "smooth"
-            })
+            });
+        } else {
+            if (window.innerWidth > 880) {
+                window.scrollTo({
+                    top: 1800,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            }
+            if (window.innerWidth > 830) {
+                window.scrollTo({
+                    top: 1750,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            } else {
+                if (window.innerWidth > 700) {
+                    window.scrollTo({
+                        top: 2100,
+                        left: 0,
+                        behavior: "smooth"
+                    });
+                } else {
+                    if (window.innerWidth > 575) {
+                        window.scrollTo({
+                            top: 2150,
+                            left: 0,
+                            behavior: "smooth"
+                        });
+                    } else {
+                        if (window.innerWidth > 400) {
+                            window.scrollTo({
+                                top: 2300,
+                                left: 0,
+                                behavior: "smooth"
+                            });
+                        } else {
+                            window.scrollTo({
+                                top: 2400,
+                                left: 0,
+                                behavior: "smooth"
+                            });
+                        }
+                    }
+                }
+            }
         }
-    else {
-        window.scrollTo({
-            top: 2050,
-            left: 0,
-            behavior: "smooth"
-        })
     }
-}
-    
-    }  
-)
+});
+
 
 const btn3 = document.querySelector('.btnProjets');
 btn3.addEventListener('click', () => {
-        if (window.innerWidth > 830) {
+    if (window.innerWidth > 1150) {
+        window.scrollTo({
+            top: 2000,
+            left: 0,
+            behavior: "smooth"
+        });
+    } else {
+        if (window.innerWidth > 960) {
             window.scrollTo({
-                top: 2000,
+                top: 2150,
                 left: 0,
                 behavior: "smooth"
-            })
+            });
         } else {
-            if (window.innerWidth > 700) {
+            if (window.innerWidth > 880) {
                 window.scrollTo({
-                    top: 2400,
+                    top: 2200,
                     left: 0,
                     behavior: "smooth"
-                })
+                });
             }
-            else {
-                if (window.innerWidth > 575) {
+            if (window.innerWidth > 830) {
+                window.scrollTo({
+                    top: 2300,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            } else {
+                if (window.innerWidth > 700) {
                     window.scrollTo({
-                        top: 2470,
+                        top: 2650,
                         left: 0,
                         behavior: "smooth"
-                    
-                    })
-                
-                
-            }else if (window.innerWidth > 400){
-                window.scrollTo({
-                    top: 2550,
-                    left: 0,
-                    behavior: "smooth"
-                })
-            }else{
-                window.scrollTo({
-                    top: 2730,
-                    left: 0,
-                    behavior: "smooth"
-                })
+                    });
+                } else {
+                    if (window.innerWidth > 575) {
+                        window.scrollTo({
+                            top: 2700,
+                            left: 0,
+                            behavior: "smooth"
+                        });
+                    } else {
+                        if (window.innerWidth > 480) {
+                            window.scrollTo({
+                                top: 2900,
+                                left: 0,
+                                behavior: "smooth"
+                            });
+
+                        } else {
+                            window.scrollTo({
+                                top: 3000,
+                                left: 0,
+                                behavior: "smooth"
+                            });
+                        }
+                    }
+                }
             }
         }
-        }
-        
-        }
-)
+    }
+});
