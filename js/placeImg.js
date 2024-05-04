cb2707b8234d6a10c2511ea661ca88590561aef2
RaphaelM <malidin.raphael@gmail.com>
2024-05-05 00:11:45 +0200

    update responsive images svg - resize

--- js/placeImg.js
+++ js/placeImg.js
@@ -4,12 +4,15 @@ window.addEventListener('load', () => {
     let imageCount;
     let imagePath = 'img/large/';
 
+
     function updateImageCount() {
         if (windowWidth > 1200) {
             imageCount = 6;
+
             
         } else if (windowWidth > 800) {
             imageCount = 4;
+
         } else {
             imageCount = 2;
         }
@@ -60,9 +63,44 @@ window.addEventListener('load', () => {
     updateImageCount();
     placeImages();
 
+
     window.addEventListener('resize', () => {
         windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
         updateImageCount();
-        placeImages();
+    
+        // Placer les images aléatoirement sur la page
+        for (let i = 1; i <= imageCount; i++) {
+            // Générer une nouvelle position aléatoire pour l'image
+            let randomX, randomY;
+            do {
+                randomX = Math.random() * (windowWidth - 200); // Calculer une position aléatoire en X
+                randomY = Math.random() * (windowHeight - 200); // Calculer une position aléatoire en Y
+            } while (collisionDetected(randomX, randomY, imagePositions));
+    
+            // Vérifier si l'image dépasse les limites de la fenêtre
+            if (randomX + 200 > windowWidth) {
+                randomX = windowWidth - 200; // Ajuster la position X pour que l'image reste entièrement visible
+            }
+            if (randomY + 200 > windowHeight) {
+                randomY = windowHeight - 200; // Ajuster la position Y pour que l'image reste entièrement visible
+            }
+    
+            // Stocker la position de cette image pour éviter les collisions
+            imagePositions.push({ x: randomX, y: randomY });
+    
+            // Définir la position de l'image
+            const img = document.createElement('img');
+            img.src = imagePath + 'image' + i + '.svg'; // Assurez-vous d'adapter cela à votre structure de fichiers
+            img.style.left = randomX + 'px';
+            img.style.top = randomY + 'px';
+            img.classList.add('imageR');
+    
+            imagesContainer.appendChild(img);
+        }
     });
+    
+    
+    
+    
 });
